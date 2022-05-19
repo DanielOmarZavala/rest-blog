11a711161a512ca230bf220668ccf7701d3c1d8d
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-05-19 15:11:38 -0500

    modified PostsController.java and completed basic CRUD functionality

--- src/main/java/com/example/restblog/web/PostsController.java
+++ src/main/java/com/example/restblog/web/PostsController.java
@@ -3,27 +3,19 @@ package com.example.restblog.web;
 import com.example.restblog.data.Post;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 public class PostsController {
 
-    List<Post> postObjectArrList = new ArrayList<>();
+    List<Post> postObjectArrList = setPostList();
 
     @GetMapping() // /api/posts/
     private List<Post> getAll() {
-
-        Post objOne = new Post(1L, "Title 1", "Desc 1");
-        Post objTwo = new Post(2L, "Title 2", "Desc 2");
-        Post objThree = new Post(3L, "Title 3", "Desc 3");
-
-        postObjectArrList.add(objOne);
-        postObjectArrList.add(objTwo);
-        postObjectArrList.add(objThree);
 
         return postObjectArrList;
     }
@@ -31,7 +23,12 @@ public class PostsController {
     @GetMapping("{id}")
     public Post getById(@PathVariable long id) {
 
-        return postObjectArrList.get((int) id);
+        for (Post post : getAll()) {
+            if (Objects.equals(post.getId(), id)) {
+                return post;
+            }
+        }
+        return null;
     }
 
     @PostMapping()
@@ -41,11 +38,30 @@ public class PostsController {
 
     @PutMapping("{id}")
     private void updatePost(@PathVariable long id, @RequestBody Post updatedPost) {
-        System.out.println(id);
+        for (Post post : postObjectArrList) {
+            if (post.getId().equals(id)) {
+                post.setContent(updatedPost.getContent());
+                post.setTitle(updatedPost.getTitle());
+            }
+        }
     }
 
     @DeleteMapping("{id}")
     private void deletePost(@PathVariable long id) {
-        System.out.println("Deleted id: " + id);
+        System.out.println("Deleted post with ID: " + id);
+    }
+
+    private List<Post> setPostList() {
+        List<Post> postList = new ArrayList<>();
+
+        Post objOne = new Post(1L, "Title 1", "Desc 1");
+        Post objTwo = new Post(2L, "Title 2", "Desc 2");
+        Post objThree = new Post(3L, "Title 3", "Desc 3");
+
+        postList.add(objOne);
+        postList.add(objTwo);
+        postList.add(objThree);
+
+        return postList;
     }
 }
